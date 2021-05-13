292cd93aa295cb130863b87c0f3a86184cfc823a
arunbhalli <tomarbhalli@gmail.com>
2021-05-14 01:09:49 +0200

    created board and game component

--- cypress/integration/tic_toe_game.js
+++ cypress/integration/tic_toe_game.js
@@ -3,9 +3,12 @@ describe("user can see his/her choice", () => {
     cy.visit('/')
   });
   it ("for selecting tick", () => {
-    cy.get("[data-cy=tick").should("contain","Rock");
+    cy.get("[data-cy=tick").should("contain","Tick");
   })
   it ("for selecting toe", () => {
-    cy.get("[data-cy=paper]").should("contain","Paper")
+    cy.get("[data-cy=toe]").should("contain","Toe")
+  })
+  it ("for selecting tac", () => {
+    cy.get("[data-cy=tac]").should("contain","Tac")
   })
 });
